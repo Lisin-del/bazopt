461dd90018bc5d3aea1a0c74b76782bf6ed096aa
Maxim Lisin <lisin.maxim.m@gmail.com>
2024-02-10 13:44:48 +0300

    initial BE configuration

---
+++ src/main/java/ru/lisin/bazopt/BazOptApplication.java
@@ -0,0 +1,11 @@
+package ru.lisin.bazopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazOptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BazOptApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/lisin/bazopt/controllers/LoginController.java
@@ -0,0 +1,11 @@
+package ru.lisin.bazopt.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/login")
+public class LoginController {
+
+
+}
