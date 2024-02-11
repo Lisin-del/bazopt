9df32f0cde57bff6b84e2d10867ba88904561262
Maxim Lisin <lisin.maxim.m@gmail.com>
2024-02-11 20:44:50 +0300

    Register page and controller

--- src/main/java/ru/lisin/bazopt/controllers/LoginController.java
+++ src/main/java/ru/lisin/bazopt/controllers/LoginController.java
@@ -1,11 +1,7 @@
 package ru.lisin.bazopt.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/login")
 public class LoginController {
-
-
 }
---
+++ src/main/java/ru/lisin/bazopt/controllers/RegistrationController.java
@@ -0,0 +1,13 @@
+package ru.lisin.bazopt.controllers;
+
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class RegistrationController {
+
+}
