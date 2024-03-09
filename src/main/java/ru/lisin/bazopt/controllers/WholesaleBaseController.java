5c70c3ce39470c706bdd8ec4a3bec1bd43e20124
Maxim Lisin <lisin.maxim.m@gmail.com>
2024-03-09 19:48:37 +0300

    wholesale base main page impl

--- src/main/java/ru/lisin/bazopt/controllers/WholesaleBaseController.java
+++ src/main/java/ru/lisin/bazopt/controllers/WholesaleBaseController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class WholesaleBaseController {
     @GetMapping("/wholesaleBaseHome")
     public String getWholesaleBaseHomePage(Model model) {
-        model.addAttribute("name", "Maxim");
+        model.addAttribute("values", List.of("hello1", "hello2", "hello3"));
         return "WholesaleBasePage";
     }
 }
